12b43324912770d5787d334386bf0e311311140a
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2017-08-22 23:19:15 +0700

    Social Login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,111 @@
 <?php
-
-
-
 /*
-
 |--------------------------------------------------------------------------
-
 | Application Routes
-
 |--------------------------------------------------------------------------
-
 |
-
 | Here is where you can register all of the routes for an application.
-
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-
 | and give it the controller to call when that URI is requested.
-
 |
-
 */
+Route::group(['prefix' => 'buyerdashboard'], function () {
+    Route::get('/', 'UserController@buyerdashboard');
+    Route::get('/changesellerpassword', 'DashboardController@changeSellerPassword');
+    Route::post('/changesellerpassword', 'DashboardController@dochangeSellerPassword');
+    Route::get('/changepmethod', 'DashboardController@changepmethod');
+    Route::post('/changepmethod', 'DashboardController@dochangepmethod');
+    Route::get('/updateprofile', 'DashboardController@updateprofile');
+    Route::post('/updateprofile', 'DashboardController@doupdateprofile');
+    Route::get('/sellitem','DashboardController@sellitem');
+    Route::post('/sellitem','DashboardController@dosellitem');
+    Route::get('/getsubcategory','DashboardController@getsubcategory');
+    Route::post('/sellstepone','ProductController@sellstepone');
+    Route::get('/sellsteptwo','ProductController@sellsteptwo');
+    Route::post('/sellsteptwo','ProductController@dosellsteptwo');
+    Route::get('/sellstepthree','ProductController@sellstepthree');
+    Route::post('/sellstepthree','ProductController@dosellstepthree');
+    Route::get('/sellpreview','ProductController@sellpreview');
+    Route::post('/sellpreview','ProductController@dosellpreview');
+    Route::get('/deletepmethod','DashboardController@deletepmethod');
+    Route::get('/addpmethod','DashboardController@addpmethod');
+    Route::post('/addpmethod','DashboardController@doaddpmethod');
+    Route::get('/hunt-an-item','DashboardController@huntanitem');
+    Route::post('/hunt-an-item','DashboardController@dohuntanitem');
+    Route::get('/messages','DashboardController@user_messages');
+    Route::post('/swapconfirmbuyer/{id}','DashboardController@swapconfirmbuyer');
+    Route::get('/view-swap-request', 'DashboardController@viewswaprequest');
+    Route::get('/view-confirm-swap-request', 'DashboardController@viewconfirmswaprequest');
+    Route::get('/sell-leads','DashboardController@sell_leads');
+    Route::get('/buy-leads','DashboardController@buy_leads');
+    Route::get('/deletemessage/{id}','DashboardController@delete_message');
+    Route::get('/manage-product-attributes','DashboardController@managepattributes');
+    Route::get('/manage-coupons', 'DashboardController@manage_coupons');
+    Route::get('/add-coupon', 'DashboardController@add_coupons');
+    Route::post('/add-coupon', 'DashboardController@doadd_coupons');
+    Route::get('/delete-coupon/{id}', 'DashboardController@delete_coupon');
+    Route::get('/update-coupon/{id}', 'DashboardController@update_coupon');
+    Route::post('/update-coupon/{id}', 'DashboardController@doupdate_coupon');
+    Route::get('/manage-rules/{id}', 'DashboardController@manage_rules');
+    Route::get('/add-rule/{id}', 'DashboardController@add_rules');
+    Route::post('/add-rule/{id}', 'DashboardController@doadd_rules');
+    Route::get('/delete-rule/{id}', 'DashboardController@delete_rules');
+    Route::get('/update-rule/{id}', 'DashboardController@update_rules');
+    Route::post('/update-rule/{id}', 'DashboardController@doupdate_rules');
+    Route::post('/hunt-product-detail/{id}', 'ProductController@hunt_product_detail');
+    Route::get('/view-hunt-request', 'DashboardController@view_hunt_request');
+    Route::get('/view-hunt/{id}', 'DashboardController@do_view_hunt_request');
+    Route::get('/confirm-hunt-buyer/{id}', 'paypalAdaptiveController@buyer_confirm_hunt');
+    Route::get('/view-confirm-hunt-request', 'DashboardController@view_confirm_hunt');
+    Route::get('/confirm-hunt-seller/{id}', 'PaypalController@seller_confirm_hunt');
+    Route::get('/seller-view-hunt/{id}', 'DashboardController@view_seller_hunt');
+    Route::get('/view-buyer-swap/{id}', 'DashboardController@view_buyer_swap');
+    Route::get('/add-attribute-groups','DashboardController@add_attribute_group');
+    Route::post('/add-attribute-groups','DashboardController@do_add_attribute_group');
+    Route::get('/add-attributes/{id}','DashboardController@add_attributes');
+    Route::post('/add-attributes/{id}','DashboardController@do_add_attributes');
+    Route::get('/getAttributes','DashboardController@get_attributes');
+    Route::get('/manage-attributes/{id}','DashboardController@manage_attributes');
+    Route::get('/delete-attributes/{id}','DashboardController@delete_attributes');
+    Route::get('/change-attribute/{id}','DashboardController@change_attribute');
+    Route::post('/change-attribute/{id}','DashboardController@do_change_attribute');
+    Route::get('/swap-transactions','DashboardController@swap_transactions');
+    Route::get('/hunt-transactions','DashboardController@hunt_transactions');
+    Route::get('/manage-shipping','DashboardController@manage_shipping');
+    Route::post('/manage-shipping','DashboardController@do_manage_shipping');
+    Route::post('/getToShipping','DashboardController@getToCountries');
+});
 
-
+Route::group(['prefix' => 'admindashboard'], function () {
+    Route::get('/','AdminController@admindashboard');
+    Route::get('/managecategories','AdminController@managecategories');
+    Route::get('/viewsubcategories','AdminController@viewsubcategories');
+    Route::get('/managebrands','AdminController@managebrands');
+    Route::get('/auctioncategories','AdminController@auctioncategories');
+    Route::get('/addproductcategory','AdminController@addproductcategory');
+    Route::post('/addproductcategory','AdminController@doaddproductcategory');
+    Route::get('/editproductcategory','AdminController@editproductcategory');
+    Route::post('/editproductcategory','AdminController@doeditproductcategory');
+    Route::get('/deleteproductcategory','AdminController@deleteproductcategory');
+    Route::get('/addbrand','AdminController@addbrand');
+    Route::post('/addbrand','AdminController@doaddbrand');
+    Route::get('/editbrands','AdminController@editbrands');
+    Route::post('/editbrands','AdminController@doeditbrands');
+    Route::get('/deletebrand','AdminController@deletebrand');
+    Route::get('/addsubcategory','AdminController@addsubcategory');
+    Route::post('/addsubcategory','AdminController@doaddsubcategory');
+    Route::get('/deletesubcategory','AdminController@deletesubcategory');
+    Route::post('/editsubcategory','AdminController@doeditsubcategory');
+    Route::get('/editsubcategory','AdminController@editsubcategory');
+    Route::get('/manage-coupons', 'AdminController@manage_coupons');
+    Route::get('/hunting-commission', 'AdminController@hunting_commission');
+    Route::get('/update-hunt-commission/{id}', 'AdminController@view_update_commission');
+    Route::post('/update-hunt-commission/{id}', 'AdminController@do_view_update_commission');
+    Route::get('/manage-reports','AdminController@manage_reports');
+    Route::get('/manage-products','AdminController@manage_products');
+    Route::get('/update-product/{id}','AdminController@update_products');
+    Route::post('/update-product/{id}','AdminController@do_update_products');
+});
 
 Route::get('/', 'UserController@index');
 Route::get('/index', 'UserController@index');
@@ -30,57 +113,21 @@ Route::get('/login', 'UserController@login');
 Route::post('/login', 'UserController@dologin');
 Route::get('/register', 'UserController@register');
 Route::post('/register', 'UserController@doregister');
-Route::get('/buyerdashboard', 'UserController@buyerdashboard');
+
 Route::get('/logout','UserController@logout');
 Route::get('/verify/{confirmationcode}/{userid}','UserController@verify');
-Route::get('/redirect', 'SocialAuthController@redirect');
-Route::get('/callback', 'SocialAuthController@callback');
-Route::get('/buyerdashboard/changesellerpassword', 'DashboardController@changeSellerPassword');
+Route::get('/redirect/{driver}', 'SocialAuthController@redirect');
+Route::get('/callback/{driver}', 'SocialAuthController@callback');
+
 
 // route for captcha created by kartik.
 Route::get('/refereshcapcha', 'UserController@refereshCapcha');
 // end captcha route here.
 
-Route::post('/buyerdashboard/changesellerpassword', 'DashboardController@dochangeSellerPassword');
-Route::get('/buyerdashboard/changepmethod', 'DashboardController@changepmethod');
-Route::post('/buyerdashboard/changepmethod', 'DashboardController@dochangepmethod');
-Route::get('/buyerdashboard/updateprofile', 'DashboardController@updateprofile');
-Route::post('/buyerdashboard/updateprofile', 'DashboardController@doupdateprofile');
-Route::get('/buyerdashboard/sellitem','DashboardController@sellitem');
-Route::post('/buyerdashboard/sellitem','DashboardController@dosellitem');
-Route::get('/buyerdashboard/getsubcategory','DashboardController@getsubcategory');
-Route::post('/buyerdashboard/sellstepone','ProductController@sellstepone');
-Route::get('/buyerdashboard/sellsteptwo','ProductController@sellsteptwo');
-Route::post('/buyerdashboard/sellsteptwo','ProductController@dosellsteptwo');
-Route::get('/buyerdashboard/sellstepthree','ProductController@sellstepthree');
-Route::post('/buyerdashboard/sellstepthree','ProductController@dosellstepthree');
-Route::get('/buyerdashboard/sellpreview','ProductController@sellpreview');
-Route::post('/buyerdashboard/sellpreview','ProductController@dosellpreview');
+
 Route::get('/admin','AdminController@adminlogin');
 Route::post('/adminlogin','AdminController@doadminlogin');
-Route::get('/admindashboard','AdminController@admindashboard');
-Route::get('/admindashboard/managecategories','AdminController@managecategories');
-Route::get('/admindashboard/viewsubcategories','AdminController@viewsubcategories');
-Route::get('/admindashboard/managebrands','AdminController@managebrands');
-Route::get('/admindashboard/auctioncategories','AdminController@auctioncategories');
-Route::get('/admindashboard/addproductcategory','AdminController@addproductcategory');
-Route::post('/admindashboard/addproductcategory','AdminController@doaddproductcategory');
-Route::get('/admindashboard/editproductcategory','AdminController@editproductcategory');
-Route::post('/admindashboard/editproductcategory','AdminController@doeditproductcategory');
-Route::get('/admindashboard/deleteproductcategory','AdminController@deleteproductcategory');
-Route::get('/admindashboard/addbrand','AdminController@addbrand');
-Route::post('/admindashboard/addbrand','AdminController@doaddbrand');
-Route::get('/admindashboard/editbrands','AdminController@editbrands');
-Route::post('/admindashboard/editbrands','AdminController@doeditbrands');
-Route::get('/admindashboard/deletebrand','AdminController@deletebrand');
-Route::get('/admindashboard/addsubcategory','AdminController@addsubcategory');
-Route::post('/admindashboard/addsubcategory','AdminController@doaddsubcategory');
-Route::get('/admindashboard/deletesubcategory','AdminController@deletesubcategory');
-Route::post('/admindashboard/editsubcategory','AdminController@doeditsubcategory');
-Route::get('/admindashboard/editsubcategory','AdminController@editsubcategory');
-Route::get('/buyerdashboard/deletepmethod','DashboardController@deletepmethod');
-Route::get('/buyerdashboard/addpmethod','DashboardController@addpmethod');
-Route::post('/buyerdashboard/addpmethod','DashboardController@doaddpmethod');
+
 // edited by kartik.
 Route::get('/listing','ProductController@listing');
 // end here.
@@ -111,16 +158,14 @@ Route::get('payment/status', array(
     'as' => 'payment.status',
     'uses' => 'paypalAdaptiveController@getPaymentStatus',
 ));
-Route::get('/buyerdashboard/hunt-an-item','DashboardController@huntanitem');
-Route::post('/buyerdashboard/hunt-an-item','DashboardController@dohuntanitem');
+
 Route::get('/hunt-listing','ProductController@huntlisting');
 Route::post('/send-buyer-message','UserController@sendmessage');
 Route::get('/product-detail/{id}','ProductController@showproductdetail');
-Route::get('/buyerdashboard/messages','DashboardController@user_messages');
+
 Route::get('/swap-listing','ProductController@swaplisting');
 Route::get('/swap-detail/{id}','ProductController@showswapdetail');
-Route::post('/buyerdashboard/swapconfirmbuyer/{id}','DashboardController@swapconfirmbuyer');
-Route::get('/buyerdashboard/view-swap-request', 'DashboardController@viewswaprequest');
+
 Route::get('/payment-swaps/{id}', array(
     'uses' => 'paypalAdaptiveController@splitPaySwapSeller',
 ));
@@ -133,7 +178,7 @@ Route::get('payment/sellerswapstatus', array(
     'as' => 'payment.sellerswapstatus',
     'uses' => 'paypalAdaptiveController@viewreportSwapSeller',
 ));
-Route::get('buyerdashboard/view-confirm-swap-request', 'DashboardController@viewconfirmswaprequest');
+
 Route::get('payment-swapb/{id}', array(
     'uses' => 'paypalAdaptiveController@splitPaySwapBuyer',
 ));
@@ -141,49 +186,28 @@ Route::get('payment/buyerswapstatus', array(
     'as' => 'payment.buyerswapstatus',
     'uses' => 'paypalAdaptiveController@viewreportSwapBuyer',
 ));
-Route::get('/buyerdashboard/sell-leads','DashboardController@sell_leads');
-Route::get('/buyerdashboard/buy-leads','DashboardController@buy_leads');
-Route::get('/buyerdashboard/deletemessage/{id}','DashboardController@delete_message');
+
 Route::get('/hunt-detail/{id}','ProductController@hunt_detail');
 Route::post('/hunt-detail/{id}','ProductController@hunt_product_detail');
-Route::get('/buyerdashboard/manage-product-attributes','DashboardController@managepattributes');
+
 Route::get('/empty-cart', 'ProductController@empty_cart');
 Route::get('/category/{id}', 'ProductController@showcategory');
 Route::get('/subcategory/{id}', 'ProductController@showsubcategory');
-Route::get('/buyerdashboard/manage-coupons', 'DashboardController@manage_coupons');
-Route::get('/buyerdashboard/add-coupon', 'DashboardController@add_coupons');
-Route::post('/buyerdashboard/add-coupon', 'DashboardController@doadd_coupons');
-Route::get('/buyerdashboard/delete-coupon/{id}', 'DashboardController@delete_coupon');
-Route::get('/buyerdashboard/update-coupon/{id}', 'DashboardController@update_coupon');
-Route::post('/buyerdashboard/update-coupon/{id}', 'DashboardController@doupdate_coupon');
-Route::get('/buyerdashboard/manage-rules/{id}', 'DashboardController@manage_rules');
-Route::get('/buyerdashboard/add-rule/{id}', 'DashboardController@add_rules');
-Route::post('/buyerdashboard/add-rule/{id}', 'DashboardController@doadd_rules');
-Route::get('/buyerdashboard/delete-rule/{id}', 'DashboardController@delete_rules');
-Route::get('/buyerdashboard/update-rule/{id}', 'DashboardController@update_rules');
-Route::post('/buyerdashboard/update-rule/{id}', 'DashboardController@doupdate_rules');
-Route::get('/admindashboard/manage-coupons', 'AdminController@manage_coupons');
+
+
 Route::get('/search', 'ProductController@search');
-Route::post('/buyerdashboard/hunt-product-detail/{id}', 'ProductController@hunt_product_detail');
-Route::get('/buyerdashboard/view-hunt-request', 'DashboardController@view_hunt_request');
-Route::get('/buyerdashboard/view-hunt/{id}', 'DashboardController@do_view_hunt_request');
-Route::get('/buyerdashboard/confirm-hunt-buyer/{id}', 'paypalAdaptiveController@buyer_confirm_hunt');
+
 Route::get('payment/buyerhuntstatus', array(
     'as' => 'payment.buyerhuntstatus',
     'uses' => 'paypalAdaptiveController@getBuyerHuntPaymentStatus',
 ));
-Route::get('/buyerdashboard/view-confirm-hunt-request', 'DashboardController@view_confirm_hunt');
-Route::get('/buyerdashboard/confirm-hunt-seller/{id}', 'PaypalController@seller_confirm_hunt');
+
 Route::get('payment/aellerhuntstatus', array(
     'as' => 'payment.sellerhuntstatus',
     'uses' => 'PaypalController@getSellerHuntPaymentStatus',
 ));
-Route::get('/buyerdashboard/seller-view-hunt/{id}', 'DashboardController@view_seller_hunt');
-Route::get('buyerdashboard/view-buyer-swap/{id}', 'DashboardController@view_buyer_swap');
-Route::get('admindashboard/hunting-commission', 'AdminController@hunting_commission');
-Route::get('admindashboard/update-hunt-commission/{id}', 'AdminController@view_update_commission');
-Route::post('admindashboard/update-hunt-commission/{id}', 'AdminController@do_view_update_commission');
-Route::get('admindashboard/manage-reports','AdminController@manage_reports');
+
+
 Route::get('adaptive', 'paypalAdaptiveController@splitPay');
 Route::get('viewreport', 'paypalAdaptiveController@viewreport');
 Route::get('manage-attributes/{id}','ProductController@manage_attributes');
@@ -200,21 +224,7 @@ Route::post('change-email','UserController@doChangeEmail');
 // prashant kumar
 Route::get('reset/{code}/{id}','UserController@changePassword');
 Route::post('reset/{code}/{id}','UserController@doChangePassword');
-Route::get('admindashboard/manage-products','AdminController@manage_products');
-Route::get('admindashboard/update-product/{id}','AdminController@update_products');
-Route::post('admindashboard/update-product/{id}','AdminController@do_update_products');
-Route::get('buyerdashboard/add-attribute-groups','DashboardController@add_attribute_group');
-Route::post('buyerdashboard/add-attribute-groups','DashboardController@do_add_attribute_group');
-Route::get('buyerdashboard/add-attributes/{id}','DashboardController@add_attributes');
-Route::post('buyerdashboard/add-attributes/{id}','DashboardController@do_add_attributes');
-Route::get('buyerdashboard/getAttributes','DashboardController@get_attributes');
-Route::get('buyerdashboard/manage-attributes/{id}','DashboardController@manage_attributes');
-Route::get('buyerdashboard/delete-attributes/{id}','DashboardController@delete_attributes');
-Route::get('buyerdashboard/change-attribute/{id}','DashboardController@change_attribute');
-Route::post('buyerdashboard/change-attribute/{id}','DashboardController@do_change_attribute');
+
+
 Route::get('invoice/{id}', 'ProductController@getInvoice');
-Route::get('buyerdashboard/swap-transactions','DashboardController@swap_transactions');
-Route::get('buyerdashboard/hunt-transactions','DashboardController@hunt_transactions');
-Route::get('buyerdashboard/manage-shipping','DashboardController@manage_shipping');
-Route::post('buyerdashboard/manage-shipping','DashboardController@do_manage_shipping');
-Route::post('buyerdashboard/getToShipping','DashboardController@getToCountries');
+
